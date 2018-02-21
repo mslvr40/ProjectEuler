a81dd1c55b7899c53184dfe532e2a3e65c398976
mslvr40 <mslvr40@aol.com>
2018-02-21 10:06:04 -0500

    Problem 2

--- Euler Problems/src/Problem2.java
+++ Euler Problems/src/Problem2.java
@@ -1,4 +1,14 @@
 
 public class Problem2 {
-
+	//Add all the even fobbonacci numbers under 4 million
+	public static void main(String[] args) {
+		int sum = 0;
+		for(int i = 1,j=2 ; j<4000000 ; j+=i,i=j-i) {
+			if(j%2==0) {
+				sum+=j;
+			}
+		}
+		System.out.println(sum);
+	}
+	
 }
